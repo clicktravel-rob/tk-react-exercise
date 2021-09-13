84b8ab6abddc1563d2a763a1ec740da11b9390b2
clicktravel-rob <Robert.Bailes@ClickTravel.com>
2021-09-13 16:25:49 +0100

    remove reg-exp from app test (and fix name)

--- recipes/src/App.test.js
+++ recipes/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders application title', () => {
   render(<App />);
-  const titleElement = screen.getByText(/Recipes Database/i);
+  const titleElement = screen.getByText('Recipes Database');
   expect(titleElement).toBeInTheDocument();
 });
